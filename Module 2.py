a63f7cafa6606b396c3125e205cd1d5d7e0368af
Diana <sandu.diana2001@gmail.com>
2023-08-31 19:12:18 +0300

    first push

--- Module 2.py
+++ Module 2.py
@@ -27,11 +27,20 @@ average = sum/3
 print(f"The average of your numbers is {average}")
 """
 #Exercise 2.5
-talent = float(input("Enter talents here:"))
-pound = float(input("Enter pounds here:"))
-lot = float(input("Enter pounds here:"))
-talent = 20*pound
-pound = 32*lot
-lot = 13.3*grams
-grams = 0.001*kilogram
-print(f"The weight you have entered is {int(kilogrmas)},{int(grams)}")
+talent_input = float(input("Enter talents here:"))
+pound_input = float(input("Enter pounds here:"))
+lot_input = float(input("Enter pounds here:"))
+
+lot_variable = 13.3
+pound_variable = 32 * lot_variable
+talent_variable = 20 * pound_variable
+
+lot_value = lot_variable * lot_input
+pound_value = pound_variable * pound_input
+talent_value = talent_variable * talent_input
+
+weight_in_g = lot_value + pound_value + talent_value
+weight_in_kg = weight_in_g / 1000
+grams = weight_in_g % 1000
+
+print(f"The weight you have entered is {int(weight_in_kg)}kg and {int(grams)}g")
