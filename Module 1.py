c8e3db383812bdc630c96718352c117e4f3ea45b
Diana <sandu.diana2001@gmail.com>
2023-08-31 16:38:17 +0300

    first push

---
+++ Module 1.py
@@ -0,0 +1,3 @@
+#Exercise 1.1
+user = input("Please enter your name here.")
+print(f"Hello {user}!")
---
+++ Module 2.py
@@ -0,0 +1,37 @@
+import math
+"""
+#Exercise 2.1
+user = input("Please enter your name here.")
+print(f"Hello {user}!")
+
+#Ecercises 2.2
+radius = float(input("Enter a radius of a circle here."))
+area = float(math.pi) * math.sqrt(radius)
+print(f"The area of your circle is = {area: .3f}")
+
+#Ecercise 2.3
+length = float(input("Enter the length of a rectangle"))
+width = float(input("Enter the width of a rectangle"))
+perimeter = 2*length + 2*width
+print(f"The perimeter of your ractangle is {perimeter: .1f}")
+
+#Exercise 2.4
+numb1 = int(input("Enter a number"))
+numb2 = int(input("Enter another number"))
+numb3 = int(input("Enter yet another number"))
+sum = numb1 + numb2 + numb3
+print(f"The sum of your numbers is {sum}")
+product = numb1 * numb2 * numb3
+print(f"The product of your numbers is {product}")
+average = sum/3
+print(f"The average of your numbers is {average}")
+"""
+#Exercise 2.5
+talent = float(input("Enter talents here:"))
+pound = float(input("Enter pounds here:"))
+lot = float(input("Enter pounds here:"))
+talent = 20*pound
+pound = 32*lot
+lot = 13.3*grams
+grams = 0.001*kilogram
+print(f"The weight you have entered is {int(kilogrmas)},{int(grams)}")
