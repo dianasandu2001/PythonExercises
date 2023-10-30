9ef658d06949765dd48f8f3b4a3242ff31c91e65
Diana <sandu.diana2001@gmail.com>
2023-10-30 20:57:36 +0200

    module 9 complete

--- Module 9/Exercise 4.py
+++ Module 9/Exercise 4.py
@@ -3,13 +3,13 @@
 # The maximum speed of each new car is a random value between 100 km/h and 200 km/h. The registration numbers are
 # created as follows: "ABC-1", "ABC-2" and so on. Now the race begins. One per every hour of the race, the following
 # operations are performed:
-        #The speed of each car is changed so that the change in speed is a random value between -10 km/h and +15 km/h.
+# The speed of each car is changed so that the change in speed is a random value between -10 km/h and +15 km/h.
 # This is done using the accerelate method.
-        #Each car is made to drive for one hour. This is done with the drive method.
+# Each car is made to drive for one hour. This is done with the drive method.
 import random
 
 
-#The race continues until one of the cars has advanced at least 10,000 kilometers. Finally, the properties of each car
+# The race continues until one of the cars has advanced at least 10,000 kilometers. Finally, the properties of each car
 # are printed out formatted into a clear table.
 class Car:
     def __init__(self, registration_num, max_speed):
@@ -24,31 +24,39 @@ class Car:
     def accelerate(self, change_speed):
         self.current_speed += change_speed
         if self.current_speed < 0 or self.current_speed > self.max_speed:
-            print("Invalid change of speed")
             if self.current_speed < 0:
                 self.current_speed = 0
             elif self.current_speed > self.max_speed:
                 self.current_speed = self.max_speed
         else:
             pass
-            print(f"The car's current's speed is now {self.current_speed}")
         return self.current_speed
 
     def drive(self, hours):
         self.travelled_distance += hours * self.current_speed
-        print(self.travelled_distance)
         return self.travelled_distance
+
+    def print_table(self):
+        print("{:<10} {:<15} {:<15} {:<15}".format("Reg Num", "Max Speed (km/h)", "Current Speed (km/h)", "Distance (km)"))
+        print("{:<10} {:<15} {:<15} {:<15}".format(car.registration_num, car.max_speed, car.current_speed, car.travelled_distance))
+
 
 car_list = []
 
 for i in range(1, 11):
     regestration_number = "ABC-" + str(i)
     max_speed = random.randint(100, 200)
-    car_list.append(Car(regestration_number,max_speed))
+    car_list.append(Car(regestration_number, max_speed))
 
 hour = 0
 race_distance = 10000
-while all(cart.travelled_distance < race_distance)):
 
-
-
+while all(car.travelled_distance < race_distance for car in car_list):
+    print("")
+    print(f"Hour {hour} of the race:")
+    print("{:<10} {:<20} {:<25} {:<20}".format("Reg Num", "Max Speed (km/h)", "Current Speed (km/h)", "Distance (km)"))
+    for car in car_list:
+        car.accelerate(random.randint(-10, 15))
+        car.drive(hour)
+        print(f"{car.registration_num:<15} {car.max_speed:<23} {car.current_speed:<20} {car.travelled_distance:<25}")
+    hour += 1
