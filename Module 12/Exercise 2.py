232bc153922fbdaf06200f203bf192d39585a05a
diana <sandu.diana2001@gmail.com>
2023-11-06 16:50:01 +0200

    Module 12 again

--- Module 12/Exercise 2.py
+++ Module 12/Exercise 2.py
@@ -5,6 +5,5 @@ import json
 keyword = input("Enter the municipality: ")
 request = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={f3030340c8d510be07322b293eecbb5f}"
 
-try:
-    response = requests.get(request).json()
-    print(response)
+response = requests.get(request).json()
+print(response)
