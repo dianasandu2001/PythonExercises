f1803a02178eefc00bbdd30bc64e73c0deafd077
diana <sandu.diana2001@gmail.com>
2023-11-06 16:15:22 +0200

    Module 11

--- Module 11/Exercise 1.py
+++ Module 11/Exercise 1.py
@@ -1,10 +1,3 @@
-#Implement the following class hierarchy using Python: A publication can be either a book or a magazine.
-# Each publication has a name. Each book also has an author and a page count, whereas each magazine has a chief editor.
-# Also write the required initializers to both classes. Create a print_information method to both subclasses for printing
-# out all information of the publication in question. In the main program, create publications Donald Duck
-# (chief editor Aki Hyyppä) and Compartment No. 6 (author Rosa Liksom, 192 pages). Print out all information of both
-# publications using the methods you implemented.
-
 class Publication:
     def __init__(self, name):
         self.name = name
--- Module 11/Exercise 2.py
+++ Module 11/Exercise 2.py
@@ -1,7 +1,83 @@
-#Extend the previosly written Car class by adding two subclasses: ElectricCar and GasolineCar. Electric cars have the
-# capacity of the battery in kilowatt-hours as their property. Gasoline cars have the volume of the tank in liters as
-# their property. Write initializers for the subclasses. For example, the initializer of electric cars receives the
-# registration number, maximum speed and battery capacity as its parameter. It calls the initializer of the base class
-# to set the first two properties and then sets its capacity. Write a main program where you create one electric car
-# (ABC-15, 180 km/h, 52.5 kWh) and one gasoline car (ACD-123, 165 km/h, 32.3 l). Select speeds for both cars, make them
-# drive for three hours and print out the values of their kilometer counters.
+import random
+
+class Car:
+    def __init__(self, registration_num, max_speed):
+        self.registration_num = registration_num
+        self.max_speed = max_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+    def print_info(self):
+        print(f"Registration number:{self.registration_num}\nMaximum speed:{self.max_speed}")
+
+    def accelerate(self, change_speed):
+        self.current_speed += change_speed
+        if self.current_speed < 0 or self.current_speed > self.max_speed:
+            if self.current_speed < 0:
+                self.current_speed = 0
+            elif self.current_speed > self.max_speed:
+                self.current_speed = self.max_speed
+        else:
+            pass
+        return self.current_speed
+
+    def drive(self, hours):
+        self.travelled_distance += hours * self.current_speed
+        return self.travelled_distance
+
+class Race(Car):
+
+    def __init__(self, name, distance, car_num):
+        self.name = name
+        self.distance = distance
+        self.car_num = car_num
+        self.car_list = []
+        for c in range(0, car_num):
+            regestration_number = "ABC-" + str(c)
+            max_speed = random.randint(100, 201)
+            self.car_list.append(Car(regestration_number, max_speed))
+
+    def hour_passes(self):
+         for car in self.car_list:
+            car.accelerate(random.randint(-10, 15))
+            car.drive(1)
+            print(f"{car.registration_num:<15} {car.max_speed:<23} {car.current_speed:<20} {car.travelled_distance:<25}")
+
+
+    def print_status(self):
+        print("{:<10} {:<20} {:<25} {:<20}".format("Reg Num", "Max Speed (km/h)", "Current Speed (km/h)", "Distance (km)"))
+
+    def race_finished(self):
+        for car in self.car_list:
+            if car.travelled_distance >= self.distance:
+                return True
+
+class Electricar(Car):
+    def __init__(self,battery_capacity,registration_num,max_speed):
+        super().__init__(registration_num,max_speed)
+        self.battery_capacity = battery_capacity
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+
+class Gasolinecar(Car):
+    def __init__(self,tank_capacity,registration_num,max_speed):
+        super().__init__(registration_num,max_speed)
+        self.tank_capacity = tank_capacity
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+cars=[]
+cars.append(Electricar("52.5 kw/h","ABC-15",180))
+cars.append(Gasolinecar("32.3 l","ACD-15", 165))
+hours=0
+
+while hours<=3:
+    for car in cars:
+        car.accelerate(random.randint(0,15))
+        car.drive(1)
+    hours+=1
+print("Registration Number","Maximum Speed", "Current Speed","Travelled Distance",sep="|")
+for car in cars:
+    car.print_info()
+
