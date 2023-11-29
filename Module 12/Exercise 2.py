e9089b0f2ed36c21930444e0c95ed02e91b370a5
diana <sandu.diana2001@gmail.com>
2023-11-29 13:55:01 +0200

    Module 12 again

--- Module 12/Exercise 2.py
+++ Module 12/Exercise 2.py
@@ -1,9 +1,15 @@
-
 import requests
 import json
 
-keyword = input("Enter the municipality: ")
-request = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={f3030340c8d510be07322b293eecbb5f}"
 
+keyword = input("Enter the Municipality: ")
+request = "https://api.openweathermap.org/data/2.5/weather?q="+keyword+"&APPID=f3030340c8d510be07322b293eecbb5f"
 response = requests.get(request).json()
-print(response)
+print("Description",response["weather"][0]['description'],sep=":")
+for i in response['main']:
+    temp=["temp","feels_like","temp_min","temp_max"]
+    if i in temp:
+        print(i, response['main'][i], sep=":", end="")
+        print("°F")
+    else:
+        print(i, response['main'][i], sep=":",)
---
+++ Module 13/Exercise 2.py
@@ -0,0 +1,52 @@
+import json
+
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host='localhost',
+    port=3307,
+    database='flight_game',
+    user='root',
+    password='dianapass',
+    autocommit=True
+)
+@app.route('/airport/<ICAO_code>')
+def airport(ICAO_code):
+    try:
+        cursor = connection.cursor()
+        statement = "select name, municipality from airport where ident = '" + ICAO_code + "'"
+        cursor.execute(statement)
+        result = cursor.fetchall()
+        for i in result:
+            print("The name of the airport is " + i[0] + " and it is located in the Town, " + i[1])
+            response = {
+                "Name" : i[0],
+                "Location" : i[1],
+                "status" : 200
+            }
+            return response
+    except ValueError:
+        response = {
+            "message": "Invalid number as addend",
+            "status": 400
+        }
+        json_response = json.dumps(response)
+        http_response = Response(response=json_response, status=400, mimetype="application/json")
+        return http_response
+
+@app.errorhandler(404)
+def page_not_found(error_code):
+    response = {
+        "message": "Invalid endpoint",
+        "status": 404
+    }
+    json_response = json.dumps(response)
+    http_response = Response(response=json_response, status=404, mimetype="application/json")
+    return http_response
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
